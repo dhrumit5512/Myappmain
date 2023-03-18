788e45b5306b8460bb7fbbc3870f9f84bffd18d9
dhrumit <dhrumitbarot1@gmail.com>
2023-03-17 18:51:41 -0600

    link those two pages for design them and add button in homescreen.

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import {Button, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
+import DetailsScreen from './DetailsScreen';
+import FrogsScreen from './FrogsScreen';
 
 function HomeScreen({ navigation }) {
   return (
