4aa44b94c57f218f18f9817f9837e43f2aaea293
dhrumit <dhrumitbarot1@gmail.com>
2023-03-19 04:31:17 -0600

    here i created card for recipe, i took card demo from online and added css for styling.

--- App.js
+++ App.js
@@ -9,7 +9,93 @@ import DetailsScreen from './DetailsScreen';
 function HomeScreen({ navigation }) {
   return (
     <SafeAreaView>
-      <ScrollView>hi
+      <ScrollView>
+        <View style={{ flex: 1 }}>
+          <Text style={styles.header}>Trending Recipes</Text>
+          <View style={styles.recipeBox}>
+            <Image source={{ uri: 'https://placekitten.com/350/150' }}
+              style={{ height: 150, resizeMode: 'contain', margin: 10, borderRadius: 10 }} />
+            <Text style={styles.homeTitle}>Chicken Tikka Masala</Text>
+            <Text style={styles.homeSub}>Chicken in an Orange Sauce</Text>
+            <Text style={styles.homeDesc}>Chicken tikka masala is a delicious dish with a thick and creamy sauce that is unforgettable after the first bite.</Text>
+            <Text style={styles.homePrep}>Prep Time: 45mins</Text>
+            <Text style={styles.homeRating}>&#9733; &#9733; &#9733; &#9733; &#9733; (123)</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            />
+          </View>
+
+          <View style={styles.recipeBox}>
+            <Image source={{ uri: 'https://placekitten.com/350/150' }}
+              style={{ height: 150, resizeMode: 'contain', margin: 10, borderRadius: 10 }} />
+            <Text style={styles.homeTitle}>Butter Chicken </Text>
+            <Text style={styles.homeSub}>Chicken in a Creamy Sauce</Text>
+            <Text style={styles.homeDesc}>Butter chicken is one of the most popular Indian dishes with westerners and Indians alike who love it for its creamy qualities and delicious flavors. </Text>
+            <Text style={styles.homePrep}>Prep Time: 49mins</Text>
+            <Text style={styles.homeRating}>&#9733; &#9733; &#9733; &#9733; &#9734; (113)</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            />
+          </View>
+
+          <View style={styles.recipeBox}>
+            <Image source={{ uri: 'https://placekitten.com/350/150' }}
+              style={{ height: 150, resizeMode: 'contain', margin: 10, borderRadius: 10 }} />
+            <Text style={styles.homeTitle}>Rogan Josh </Text>
+            <Text style={styles.homeSub}>Lamb Curry</Text>
+            <Text style={styles.homeDesc}>lamb chunks cooked with a gravy made from browned onions, yogurt, garlic, ginger, and aromatic spices such as cloves, bay leaves, cardamom, and cinnamon.</Text>
+            <Text style={styles.homePrep}>Prep Time: 80mins</Text>
+            <Text style={styles.homeRating}>&#9733; &#9733; &#9733; &#9733; &#9734; (89)</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            />
+          </View>
+
+                    <View style={styles.recipeBox}>
+            <Image source={{ uri: 'https://placekitten.com/350/150' }}
+              style={{ height: 150, resizeMode: 'contain', margin: 10, borderRadius: 10 }} />
+            <Text style={styles.homeTitle}>Chana Masala </Text>
+            <Text style={styles.homeSub}>Chickpea Curry</Text>
+            <Text style={styles.homeDesc}>Sometimes called chole bhature, this much-loved street food and home-cooked dish is one of the most popular in the country. </Text>
+            <Text style={styles.homePrep}>Prep Time: 30mins</Text>
+            <Text style={styles.homeRating}>&#9733; &#9733; &#9733; &#9734; &#9734; (88)</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            />
+          </View>
+
+          <View style={styles.recipeBox}>
+            <Image source={{ uri: 'https://placekitten.com/350/150' }}
+              style={{ height: 150, resizeMode: 'contain', margin: 10, borderRadius: 10 }} />
+            <Text style={styles.homeTitle}>Biryani</Text>
+            <Text style={styles.homeSub}>Spiced Rice with Meat</Text>
+            <Text style={styles.homeDesc}>Biryani is an all-time favorite food amongst Indians. This is because there are approximately a hundred types of biryani and every one of them tastes heavenly.</Text>
+            <Text style={styles.homePrep}>Prep Time: 15mins</Text>
+            <Text style={styles.homeRating}>&#9733; &#9733; &#9733; &#9734; &#9734; (122)</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            />
+          </View>
+
+          <View style={styles.recipeBox}>
+            <Image source={{ uri: 'https://placekitten.com/350/150' }}
+              style={{ height: 150, resizeMode: 'contain', margin: 10, borderRadius: 10 }} />
+            <Text style={styles.homeTitle}>Masala Dosa</Text>
+            <Text style={styles.homeSub}>Thin Crepe Served with Sauces</Text>
+            <Text style={styles.homeDesc}>A dosa is a thin bread that is cooked similarly to a crepe. But, instead of the usual flour, its main ingredients are fermented rice and black lentils. </Text>
+            <Text style={styles.homePrep}>Prep Time: 40mins</Text>
+            <Text style={styles.homeRating}>&#9733; &#9733; &#9733; &#9733; &#9734; (12)</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            />
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -19,7 +105,7 @@ function BlogScreen({ navigation }) {
   return (
     <SafeAreaView>
       <ScrollView>
-        hi
+      
       </ScrollView>
     </SafeAreaView>
   );
@@ -28,18 +114,17 @@ function BlogScreen({ navigation }) {
 function SavedScreen({ navigation }) {
   return (
     <SafeAreaView>
-      <ScrollView>hi
+      <ScrollView>
+
       </ScrollView>
     </SafeAreaView>
   );
 }
 
-
 function SettingsScreen({ navigation }) {
   return (
     <SafeAreaView>
       <ScrollView>
-        
       </ScrollView>
     </SafeAreaView>
   );
@@ -133,6 +218,48 @@ function SettingsStackScreen() {
     </SettingsStack.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    color: 'black',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+  },
+  recipeBox: {
+    backgroundColor: 'orange',
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  news: {
+    margin: 20,
+  },
+  newsBody: {
+    fontSize: 15,
+    marginBottom: 10,
+  },
+  homeTitle: {
+    fontSize: 22,
+    fontWeight: 'bold'
+  },
+  homeSub: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  homeDesc: {
+    fontSize: 14,
+  },
+
+  homePrep: {
+    fontWeight: 'bold',
+    fontSize: 13,
+  },
+  homeRating: {
+    fontSize: 16,
+  },
+})
 
 const Tab = createBottomTabNavigator();
 
