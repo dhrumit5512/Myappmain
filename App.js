06a54a46e9545fcdb4b053907a44f20dacf460e8
dhrumit <dhrumitbarot1@gmail.com>
2023-03-19 21:03:34 -0600

    copy same card 6x and add more styling in card.

--- App.js
+++ App.js
@@ -25,6 +25,76 @@ function HomeScreen({ navigation }) {
               onPress={() => navigation.navigate('Details')}
             />
           </View>
+
+          <View style={styles.recipeBox}>
+            <Image source={{ uri: 'https://picsum.photos/200/300' }}
+              style={{ height: 150, resizeMode: 'contain', margin: 10, borderRadius: 10 }} />
+            <Text style={styles.homeTitle}>Butter Chicken </Text>
+            <Text style={styles.homeSub}>Chicken in a Creamy Sauce</Text>
+            <Text style={styles.homeDesc}>Butter chicken is one of the most popular Indian dishes with westerners and Indians alike who love it for its creamy qualities and delicious flavors. </Text>
+            <Text style={styles.homePrep}>Prep Time: 49mins</Text>
+            <Text style={styles.homeRating}>&#9733; &#9733; &#9733; &#9733; &#9734; (113)</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            />
+          </View>
+
+          <View style={styles.recipeBox}>
+            <Image source={{ uri: 'https://picsum.photos/200/300' }}
+              style={{ height: 150, resizeMode: 'contain', margin: 10, borderRadius: 10 }} />
+            <Text style={styles.homeTitle}>Rogan Josh </Text>
+            <Text style={styles.homeSub}>Lamb Curry</Text>
+            <Text style={styles.homeDesc}>lamb chunks cooked with a gravy made from browned onions, yogurt, garlic, ginger, and aromatic spices such as cloves, bay leaves, cardamom, and cinnamon.</Text>
+            <Text style={styles.homePrep}>Prep Time: 80mins</Text>
+            <Text style={styles.homeRating}>&#9733; &#9733; &#9733; &#9733; &#9734; (89)</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            />
+          </View>
+
+                    <View style={styles.recipeBox}>
+            <Image source={{ uri: 'https://picsum.photos/200/300' }}
+              style={{ height: 150, resizeMode: 'contain', margin: 10, borderRadius: 10 }} />
+            <Text style={styles.homeTitle}>Chana Masala </Text>
+            <Text style={styles.homeSub}>Chickpea Curry</Text>
+            <Text style={styles.homeDesc}>Sometimes called chole bhature, this much-loved street food and home-cooked dish is one of the most popular in the country. </Text>
+            <Text style={styles.homePrep}>Prep Time: 30mins</Text>
+            <Text style={styles.homeRating}>&#9733; &#9733; &#9733; &#9734; &#9734; (88)</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            />
+          </View>
+
+          <View style={styles.recipeBox}>
+            <Image source={{ uri: 'https://picsum.photos/200/300' }}
+              style={{ height: 150, resizeMode: 'contain', margin: 10, borderRadius: 10 }} />
+            <Text style={styles.homeTitle}>Biryani</Text>
+            <Text style={styles.homeSub}>Spiced Rice with Meat</Text>
+            <Text style={styles.homeDesc}>Biryani is an all-time favorite food amongst Indians. This is because there are approximately a hundred types of biryani and every one of them tastes heavenly.</Text>
+            <Text style={styles.homePrep}>Prep Time: 15mins</Text>
+            <Text style={styles.homeRating}>&#9733; &#9733; &#9733; &#9734; &#9734; (122)</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            />
+          </View>
+
+          <View style={styles.recipeBox}>
+            <Image source={{ uri: 'https://picsum.photos/200/300' }}
+              style={{ height: 150, resizeMode: 'contain', margin: 10, borderRadius: 10 }} />
+            <Text style={styles.homeTitle}>Masala Dosa</Text>
+            <Text style={styles.homeSub}>Thin Crepe Served with Sauces</Text>
+            <Text style={styles.homeDesc}>A dosa is a thin bread that is cooked similarly to a crepe. But, instead of the usual flour, its main ingredients are fermented rice and black lentils. </Text>
+            <Text style={styles.homePrep}>Prep Time: 40mins</Text>
+            <Text style={styles.homeRating}>&#9733; &#9733; &#9733; &#9733; &#9734; (12)</Text>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            />
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
